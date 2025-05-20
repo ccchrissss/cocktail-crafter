198e47ece964c58fc4b84378f01c73680a3c04c0
Chris Nelson <chrisnelson7791@gmail.com>
2025-05-20 14:38:40 -0700

    feat: update title and cocktail question, including wrapping trick for question. Add hr element. Fix margin and padding between text elements to more closely match Cocktail Connoisseur 1. Add padding around hero-content div to match padding in Navbar.

--- src/components/CocktailPicker.jsx
+++ src/components/CocktailPicker.jsx
@@ -4,22 +4,29 @@ const CocktailPicker = () => {
 
   return (
     <>
-      <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src={cocktailInHand}
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
-        <div>
-          <h1 className="text-5xl font-bold">The Cocktail Picker</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-            quasi. In deleniti eaque aut repudiandae et a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+      <div className="hero bg-base-200 min-h-[400px]">
+        <div className="hero-content flex-col lg:flex-row-reverse p-6">
+          <img
+            src={cocktailInHand}
+            className="hidden rounded-lg shadow-2xl m-8 lg:block"
+          />
+          <div>
+            <div className="flex justify-center flex-wrap">
+              <h1 className="text-4xl text-center font-bold my-6">The Cocktail Picker</h1>
+              <div className="h-0 basis-full"></div>
+              <hr className="border border-neutral-400 w-40 my-6" />
+              <div className="h-0 basis-full"></div>
+              <p className="text-center py-6">
+              Which cocktail would you like to make&nbsp;today?
+              </p>
+            </div>
+            <div className="flex justify-center max-w-88 m-auto">
+              <input type="text" placeholder="e.g. margarita" className="input mr-2 mb-6" />
+              <button className="btn btn-primary mb-6">Get Cocktail</button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
     </>
   )
 
