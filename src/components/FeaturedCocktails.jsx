474ca958c92ad1426eb3cd02b61e966a9e8288f1
Chris Nelson <chrisnelson7791@gmail.com>
2025-06-14 12:50:26 -0700

    feat: restyle caption/banner for Garibaldi Negroni with a darkened, blurred overlay. Add anchor link to the heading. Apply to all Featured Cocktails drinks. Also, change order of some of the drinks.

--- src/components/FeaturedCocktails.jsx
+++ src/components/FeaturedCocktails.jsx
@@ -26,61 +26,90 @@ const FeaturedCocktails = () => {
   return (
     <>
       <h3 className="p-6 text-xl">Featured Cocktails</h3>
-      <div className="relative group">
+      <div className="relative group/arrow-btns">
         <div ref={carouselRef} className="relative carousel carousel-center rounded-box max-h-60">
           {/* <div className="top-1/2 absolute left-5 right-5 flex transform justify-between ">
             <a className="btn btn-square" onClick={() => handleClick(-1)}>❮</a>
             <a className="btn btn-circle" onClick={() => handleClick(1)}>❯</a>
           </div> */}
-          <div className="carousel-item relative max-w-[240px] py-3 px-3 group">
+          <div className="carousel-item relative max-w-[240px] py-3 px-3 group/garibaldi">
             <img
               className="mx-auto rounded-md"
               src={garibaldiNegroni}
               alt="Garibaldi Negroni"
             />
-            <div className="absolute w-[216px] bottom-[25%] h-[15%] mx-3 flex justify-center items-center bg-black/50 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <h3 className="cursor-pointer">Garibaldi Negroni</h3>
+            <div className="absolute w-[216px] bottom-3 h-[25%] mx-3 flex justify-center hover: items-center text-l font text-white rounded-b-md border-t-1 border-white backdrop-brightness-87 font-medium backdrop-blur-[5px]">
+              <h3 className="cursor-pointer">
+              <a className="link link-hover">Garibaldi Negroni</a></h3>
             </div>
           </div>
           <div className="carousel-item max-w-[240px] py-3 px-3">
             <img className="mx-auto rounded-md"
               src={grimReaper}
               alt="Grim Reaper" />
-          </div>
-          <div className="carousel-item max-w-[240px] py-3 px-3">
-            <img className="mx-auto rounded-md"
-              src={b52}
-              alt="B 52" />
-          </div>
-          <div className="carousel-item max-w-[240px] py-3 px-3">
-            <img className="mx-auto rounded-md"
-                src={ginLemon}
-                alt="Gin Lemon" />
-          </div>
-          <div className="carousel-item max-w-[240px] py-3 px-3">
-            <img className="mx-auto rounded-md"
-              src={greyhound}
-              alt="Greyhound" />
-          </div>
-          <div className="carousel-item max-w-[240px] py-3 px-3">
-            <img className="mx-auto rounded-md"
-                src={mountainBramble}
-                alt="Mountain Bramble" />
+            <div className="absolute w-[216px] bottom-3 h-[25%] mx-3 flex justify-center hover: items-center text-l font text-white rounded-b-md border-t-1 border-white backdrop-brightness-87 font-medium backdrop-blur-[5px]">
+              <h3 className="cursor-pointer">
+              <a className="link link-hover">Grim Reaper</a></h3>
+            </div>
           </div>
           <div className="carousel-item max-w-[240px] py-3 px-3">
             <img className="mx-auto rounded-md"
                 src={thePhilosopher}
                 alt="The Philosopher" />
+            <div className="absolute w-[216px] bottom-3 h-[25%] mx-3 flex justify-center hover: items-center text-l font text-white rounded-b-md border-t-1 border-white backdrop-brightness-87 font-medium backdrop-blur-[5px]">
+              <h3 className="cursor-pointer">
+              <a className="link link-hover">The Philosopher</a></h3>
+            </div>
+          </div>
+          <div className="carousel-item max-w-[240px] py-3 px-3">
+            <img className="mx-auto rounded-md"
+                src={ginLemon}
+                alt="Gin Lemon" />
+            <div className="absolute w-[216px] bottom-3 h-[25%] mx-3 flex justify-center hover: items-center text-l font text-white rounded-b-md border-t-1 border-white backdrop-brightness-87 font-medium backdrop-blur-[5px]">
+              <h3 className="cursor-pointer">
+              <a className="link link-hover">Gin Lemon</a></h3>
+            </div>
+          </div>
+          <div className="carousel-item max-w-[240px] py-3 px-3">
+            <img className="mx-auto rounded-md"
+                src={mountainBramble}
+                alt="Mountain Bramble" />
+            <div className="absolute w-[216px] bottom-3 h-[25%] mx-3 flex justify-center hover: items-center text-l font text-white rounded-b-md border-t-1 border-white backdrop-brightness-87 font-medium backdrop-blur-[5px]">
+              <h3 className="cursor-pointer">
+              <a className="link link-hover">Mountain Bramble</a></h3>
+            </div>
+          </div>
+          <div className="carousel-item max-w-[240px] py-3 px-3">
+            <img className="mx-auto rounded-md"
+              src={greyhound}
+              alt="Greyhound" />
+            <div className="absolute w-[216px] bottom-3 h-[25%] mx-3 flex justify-center hover: items-center text-l font text-white rounded-b-md border-t-1 border-white backdrop-brightness-87 font-medium backdrop-blur-[5px]">
+              <h3 className="cursor-pointer">
+              <a className="link link-hover">Greyhound</a></h3>
+            </div>
+          </div>
+          <div className="carousel-item max-w-[240px] py-3 px-3">
+            <img className="mx-auto rounded-md"
+              src={b52}
+              alt="B 52" />
+            <div className="absolute w-[216px] bottom-3 h-[25%] mx-3 flex justify-center hover: items-center text-l font text-white rounded-b-md border-t-1 border-white backdrop-brightness-87 font-medium backdrop-blur-[5px]">
+              <h3 className="cursor-pointer">
+              <a className="link link-hover">B 52</a></h3>
+            </div>
           </div>
           <div className="carousel-item max-w-[240px] py-3 px-3">
             <img className="mx-auto rounded-md"
               src={vampiro}
               alt="Vampiro" />
+            <div className="absolute w-[216px] bottom-3 h-[25%] mx-3 flex justify-center hover: items-center text-l font text-white rounded-b-md border-t-1 border-white backdrop-brightness-87 font-medium backdrop-blur-[5px]">
+              <h3 className="cursor-pointer">
+              <a className="link link-hover">Vampiro</a></h3>
+            </div>
           </div>
         </div>
         <div className="top-[42%] absolute left-7 right-7 flex transform justify-between">
-          <a className="btn btn-circle hidden group-hover:inline-flex" onClick={() => handleClick(-1)}>❮</a>
-          <a className="btn btn-circle hidden group-hover:inline-flex" onClick={() => handleClick(1)}>❯</a>
+          <a className="btn btn-circle opacity-0 group-hover/arrow-btns:opacity-100 transition-opacity duration-300" onClick={() => handleClick(-1)}>❮</a>
+          <a className="btn btn-circle hidden group-hover/arrow-btns:inline-flex" onClick={() => handleClick(1)}>❯</a>
         </div>
       </div>
     </>
