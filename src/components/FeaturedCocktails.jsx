a965d923edf9d37bd07c0a8cc149115e251acff0
Chris Nelson <chrisnelson7791@gmail.com>
2026-02-28 16:25:14 -0800

    hotfix: comment out old visibleInViewport logic. Add overscroll styling to carouel.

--- src/components/FeaturedCocktails.jsx
+++ src/components/FeaturedCocktails.jsx
@@ -69,7 +69,7 @@ const FeaturedCocktails = ({ handleGreyhoundClick, handleVampiroClick, handleThe
 
   const lastCarouselElCheck = () => {
 
-    !isOutermostSideVisibleInViewport(carouselLastElRef, 'right') ? setRightArrowBtnDisabled(false) : setRightArrowBtnDisabled(true)
+    // !isOutermostSideVisibleInViewport(carouselLastElRef, 'right') ? setRightArrowBtnDisabled(false) : setRightArrowBtnDisabled(true)
 
     isOutermostSideVisibleInViewport(carouselLastElRef, 'right') > isOutermostSideVisibleInViewport(carouselRef, 'right') ? setRightArrowBtnDisabled(false) : setRightArrowBtnDisabled(true)
   
@@ -281,7 +281,7 @@ const FeaturedCocktails = ({ handleGreyhoundClick, handleVampiroClick, handleThe
           <h3 id="featured-cocktails" className="p-6 text-xl">Featured Cocktails</h3>
           <div className="h-0 basis-full"></div>
           <div className="relative group/arrow-btns">
-            <div ref={carouselRef} className="relative carousel carousel-center rounded-box max-h-60 md:mx-6" onScroll={handleCarouselScroll}>
+            <div ref={carouselRef} className="relative carousel carousel-center overscroll-none rounded-box max-h-60 md:mx-6" onScroll={handleCarouselScroll}>
               {/* <div className="top-1/2 absolute left-5 right-5 flex transform justify-between ">
                 <a className="btn btn-square" onClick={() => handleClick(-1)}>❮</a>
                 <a className="btn btn-circle" onClick={() => handleClick(1)}>❯</a>
